2ab1c3d09dcd0d61d9b346aafc027770b92bc97f
Camila Vital <camsvital@gmail.com>
2023-05-26 21:48:38 -0300

    renderização dos posters do filme

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,73 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function HomePage() {
-    return (
-        <PageContainer>
-            Selecione o filme
+  const [imgFilme, setImgFilme] = useState([]);
 
-            <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+  useEffect(() => {
+    const URL = "https://mock-api.driven.com.br/api/v8/cineflex/movies";
+    const promise = axios.get(URL);
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+    promise.then((listaFilmes) => {
+      console.log(listaFilmes.data);
+      setImgFilme(listaFilmes.data);
+    });
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+    promise.catch((erro) => {
+      console.log(erro.reponse.data);
+    });
+  }, []);
+ 
+if (imgFilme.length === 0){
+  return (
+    <div>carregando</div> //substituir por gif
+  )
+}
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-            </ListContainer>
-
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      Selecione o filme
+      <ListContainer key={imgFilme.id}>
+        {imgFilme.map((imgFilme) => (
+          <MovieContainer>
+            <img src={imgFilme.posterURL} alt="poster" />
+          </MovieContainer>
+        ))}
+      </ListContainer>
+    </PageContainer>
+  );
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-top: 70px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+  color: #293845;
+  margin-top: 30px;
+  padding-top: 70px;
+`;
 const ListContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    padding: 10px;
-`
+  width: 330px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 10px;
+`;
 const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
-`
+  width: 145px;
+  height: 210px;
+  box-shadow: 0px 2px 4px 2px #0000001a;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  img {
+    width: 130px;
+    height: 190px;
+  }
+`;
